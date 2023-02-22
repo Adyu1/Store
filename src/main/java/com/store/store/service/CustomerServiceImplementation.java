5bae5bf95b122b529f764d5e705fc4f6e2f65ead
Adyu1 <adring17c@gmail.com>
2023-02-22 11:01:28 +0100

    DTO created

--- src/main/java/com/store/store/service/CustomerServiceImplementation.java
+++ src/main/java/com/store/store/service/CustomerServiceImplementation.java
@@ -1,10 +1,13 @@
 package com.store.store.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.store.store.dto.CustomerDto;
 import com.store.store.model.Customer;
 import com.store.store.repository.CustomerRepository;
 
@@ -20,9 +23,11 @@ public class CustomerServiceImplementation implements CustomerService {
         return customer.saveCustomer(customer);
     }
 
-    @Override
-    public List<Customer> getAllCustomers(){
-        return customerRepository.findAll();
+   @Override
+    public List<CustomerDto> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream()
+                        .map(customer -> modelMapper.map(customer, CustomerDto.class))
+                        .collect(Collectors.toList());
     }
-    
 }
